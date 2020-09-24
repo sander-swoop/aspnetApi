129efb227dcda7904897328503a05edc4855b510
Dani Scarpa <daniscarpa8593@gmail.com>
2020-09-24 22:55:48 +0100

    ASP.NET Api App with Dota2 :)
    Added the first enpoint: Hero :))

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using RestSharp;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace helloworld_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeroController : ControllerBase
+    {
+        private readonly ILogger<HeroController> _logger;
+
+        public HeroController(ILogger<HeroController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Hero> Get()
+        {
+            var client = new RestClient("https://api.opendota.com/api");
+            var request = new RestRequest("heroes", DataFormat.Json);
+
+            var response = client.Get<List<Hero>>(request).Data;
+
+            return response;
+        }
+    }
+}
---
+++ Hero.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace helloworld_api
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Localized_Name { get; set; }
+        public string Primary_Attr { get; set; }
+        public string Attack_Type { get; set; }
+        public List<string> Roles { get; set; }
+        public int Legs { get; set; }
+    }
+}
